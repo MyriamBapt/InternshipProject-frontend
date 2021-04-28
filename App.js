a84d47dab1d14120935c356d8de9a0db62bfc345
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-04-28 15:41:33 +0200

    app.js modifications for prolisting tests

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import ProListing from "./src/screens/pro-listing/pro-listing";
 
 const App: () => Node = () => {
 
   return (
+    <View>
     <Text>C'est en travaux, ça arrive !</Text>
+      <ProListing/>
+    </View>
   )}
 export default App;

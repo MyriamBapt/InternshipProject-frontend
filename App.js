05b7bf65cf5f1750d46a377831454aaa57f7bc9f
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-28 17:22:06 +0200

    adding provider and store in the App

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
 import { Text, View } from "react-native";
+
+import { Provider } from 'react-redux';
+import store from './src/store'
+
 import ProListing from "./src/screens/pro-listing/pro-listing";
 
+// @ts-ignore
 const App: () => Node = () => {
 
   return (
-    <View>
-    <Text>C'est en travaux, ça arrive !</Text>
-      <ProListing/>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>C'est en travaux, ça arrive !</Text>
+        <ProListing/>
+      </View>
+    </Provider>
   )}
 export default App;

0e8bc05af765aa8d5d754fd79b82762adbe189ca
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-04-29 16:34:20 +0200

    adding navigation

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-// import 'react-native-gesture-handler'; (makes everything bug when i run app)
+import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
-import { Text, View } from "react-native";
 
 import { Provider } from 'react-redux';
 import store from './src/store'
 
 import ProListing from "./src/screens/pro-listing/pro-listing";
+import { NavigationContainer } from "@react-navigation/native";
+import Stack from "./src/router/navigation";
+import ProProfile from "./src/screens/pro-profile/pro-profile";
+import ProAllReviews from "./src/screens/pro-all-reviews/pro-all-reviews";
 
 // @ts-ignore
 const App: () => Node = () => {
 
   return (
     <Provider store={store}>
-      <View>
-        <Text>C'est en travaux, ça arrive !</Text>
-      </View>
-      <ProListing/>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Professionals">
+          <Stack.Screen name="Professionnals" component={ProListing} options={{ title: 'Find professionnals',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 25,
+              letterSpacing: 0.12, }, }} />
+          <Stack.Screen name="Profile" component={ProProfile} options={{ title: '',
+            headerStyle: {
+              backgroundColor: '#4EBDD6', },
+            headerTintColor: '#fff',}}  />
+          <Stack.Screen name="Reviews" component={ProAllReviews} options={{ title: '',
+            headerStyle: {
+              backgroundColor: '#4EBDD6', },
+            headerTintColor: '#fff',}}  />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   )}
 export default App;

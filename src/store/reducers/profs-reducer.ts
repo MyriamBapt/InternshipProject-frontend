926c3030445c0d15aa4f30a2909bdb297fc49ad8
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-05 16:09:58 +0200

    correcting reducer

--- src/store/reducers/profs-reducer.ts
+++ src/store/reducers/profs-reducer.ts
@@ -1,9 +1,8 @@
-// @ts-ignore
 import { Action, handleActions } from 'redux-actions';
 import * as types from '../types';
 
 export interface IProfState {
-  profs: any[] | null,
+  profs: any[] | undefined,
   prof: any | undefined,
   loading: boolean,
   error: string
@@ -11,23 +10,23 @@ export interface IProfState {
 
 const initialState: IProfState = {
   profs: [],
-  prof: undefined,
+  prof: {},
   loading: false,
   error: ''
 }
 
 export const profsReducer = handleActions<IProfState, any>(
   {
-    [types.FETCH_ALL_PROFS_REQUEST]: (state: any, action: Action<any>) => ({
+    [types.FETCH_ALL_PROFS_REQUEST]: (state: any = initialState, action: Action<any>) => ({
       ...state,
       loading: true,
     }),
-    [types.FETCH_ALL_PROFS_SUCCESS]: (state: any, action: Action<any>) => ({
+    [types.FETCH_ALL_PROFS_SUCCESS]: (state: any = initialState, action: Action<any>) => ({
       ...state,
       loading: false,
       profs: action.payload,
     }),
-    [types.FETCH_ALL_PROFS_FAILURE]: (state: any, action: Action<any>) => ({
+    [types.FETCH_ALL_PROFS_FAILURE]: (state: any = initialState, action: Action<any>) => ({
       ...state,
       loading: false,
       error: action.payload

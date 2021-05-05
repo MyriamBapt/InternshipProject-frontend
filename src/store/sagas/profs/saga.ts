b28a0995a9617c757d654f52671c82c48bed9435
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-05 16:09:34 +0200

    adding fetchAllProfsSaga

--- src/store/sagas/profs/saga.ts
+++ src/store/sagas/profs/saga.ts
@@ -1,11 +1,19 @@
-import {AnyAction} from 'redux';
-import {call, put, takeLatest} from 'redux-saga/effects';
-import * as actions from '../../actions';
+import { AnyAction } from "redux";
+import { call, put, takeLatest } from "redux-saga/effects";
+import * as actions from "../../actions";
+import { getAllProfs } from "../../../api/services/profs";
+import { fetchAllProfsFailure, fetchAllProfsSuccess } from "../../actions";
 
-export function* getAllProfsSaga(action: AnyAction) {
-  return null;
+export function* fetchAllProfsSaga(action: AnyAction) {
+  try {
+    // @ts-ignore
+    const response: any = yield call(getAllProfs);
+    yield put(fetchAllProfsSuccess(response));
+  } catch (error) {
+    yield put(fetchAllProfsFailure(error.message));
+  }
 }
 
-export default function* Saga() {
-  yield takeLatest(actions.fetchAllProfsRequest, getAllProfsSaga)
+export default function* profSaga() {
+  yield takeLatest(actions.fetchAllProfsRequest, fetchAllProfsSaga);
 }

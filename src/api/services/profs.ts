23a6ce5a0dfcc1f56934440c130323f84e2fc811
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-07 15:14:45 +0200

    updating get url for profs

--- src/api/services/profs.ts
+++ src/api/services/profs.ts
@@ -1,6 +1,6 @@
 import makeRequest from "../index";
 
-const allProfsUrl = `/professional/all_with_review`;
+const allProfsUrl = `/professional/all_with_tag_language`;
 
 export async function getAllProfs(): Promise<any> {
   const result = await makeRequest.get(allProfsUrl);

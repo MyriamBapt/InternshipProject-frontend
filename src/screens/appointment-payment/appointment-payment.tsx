12097519ec2635ff07098c86488beb1f31a3571a
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-05-07 16:09:05 +0200

    useSelector added

--- src/screens/appointment-payment/appointment-payment.tsx
+++ src/screens/appointment-payment/appointment-payment.tsx
@@ -3,12 +3,24 @@ import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
 import AppointmentInfoTop from "../../components/appointment-info-top/appointment-info-top";
 import styles from "./styles";
 import SimpleButton from "../../components/simple-button/simple-button";
+import { useNavigation } from "@react-navigation/native";
+import { ProfessionalModel } from "../../api/models/professional.model";
+import { useSelector } from "react-redux";
+import { IProfState } from "../../store/reducers/profs-reducer";
 
 interface AppointmentPaymentProps {
   //prof: ProfModel
+  route: any;
 }
 
 const AppointmentPayment: FC<AppointmentPaymentProps> = (props: AppointmentPaymentProps) => {
+
+  const navigation = useNavigation();
+  //const { id } = props.route.params;
+  const id = 9;
+  //attention: need to come from previous screen otherwise useSelector returns undefined (need dispatch from app-date screen)
+  // @ts-ignore
+  const prof: ProfessionalModel = useSelector((state: IProfState) => state.profs.profs.find(prof => prof.id === id));
 
   const [dataValidated, setDataValidated]= useState(true);
 

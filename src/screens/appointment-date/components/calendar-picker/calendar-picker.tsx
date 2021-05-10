8424fc1f147868dcf743c96c7b1de33edb07f1ca
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-05-10 18:08:53 +0200

    testing a new calendar (in progress)

---
+++ src/screens/appointment-date/components/calendar-picker/calendar-picker.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+interface CalendarPickerProps {
+
+}
+
+const CalendarPicker: FC<CalendarPickerProps> = (props: CalendarPickerProps) => {
+
+}
+
+export default CalendarPicker;
